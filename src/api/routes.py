0ebab308220d504a20280a4b0d34bcb475e63f3f
Ana Pacheco <kornetera@gmail.com>
2023-04-20 09:53:41 +0000

    example get all users

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,21 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models.index import db, User
+from api.models.index import db, User, Company, Favorites,Lawyer_review, Lawyer_review_comment, Lawyer_review, Lawyer, Question_comment, Question, Review_comment,Review, Roles
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/hello', methods=['GET'])
 def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+    
+    all_users = User.query.all()
+    user_serialized = list( 
+        map(lambda user: user.serialize(), all_users))
+    response = {
+            "result": {
+                "users": user_serialized
+            }
+        }
+    return response, 200
