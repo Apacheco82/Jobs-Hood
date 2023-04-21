217e0f2ac61f2167c4ef6c8a08bb3a1a14fa891c
Hedonista17 <andreaynereapiza@gmail.com>
2023-04-21 14:55:32 +0000

    Verificacion de len de password creada en archiivo functions.py

--- src/api/functions.py
+++ src/api/functions.py
@@ -19,5 +19,11 @@ def verificar_usuario(new_user):
     for field, error_msg in required_fields.items():
         if new_user[field] is None or new_user[field] == "":
             return {"msg": error_msg, "error": True, "status": 400}
+        
+        if field == 'password' and len(new_user[field]) < 8:
+            return {"msg": "La contraseña debe tener al menos 8 caracteres", "error": True, "status": 400}
+        
+        if field == 'password' and len(new_user[field]) > 20 :
+            return {"msg": "La contraseña debe ser como máximo de 20 caracteres", "error": True, "status": 400}
 
     return new_user
