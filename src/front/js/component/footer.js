f2809444509753256b344ca83caaeba458391137
BilelMatallah <107211669+BilelMatallah@users.noreply.github.com>
2023-05-01 12:15:45 +0200

    feat: nav y footer terminados. (falta acabar diseño general de la pagina) (#34)

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa"; /* Para tener acceso a React-icons ejecutamos "npm install react-icons --save" */
+import { FaLinkedin } from "react-icons/fa";
+import { FaGooglePlusG } from "react-icons/fa";
+
+
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	
+<footer>
+	
+		<div className="container">
+			<div className="d-flex pt-4 pb-4 footer_body">
+				<div className="social">
+					<div className="icon-circle text-center d-flex gap-3">
+						<a href="#"><FaFacebookF className="ico facebook"/></a>
+						<a href="#"><FaTwitter className="ico twitter"/></a>
+						<a href="#"><FaLinkedin className="ico linkedin"/></a>
+						<a href="#"><FaGooglePlusG className="ico google"/></a>
+					</div>				
+				</div>
+
+				<div className="priv_contact">
+					<div className="privacy">
+						<a href="#"><h4>Política de privacidad</h4></a> 
+					</div>
+
+					<div className="contact">
+						<a href="#"><h4>Contacto</h4></a>  
+					</div>
+				</div>
+
+			</div>
+		</div>
+	
+</footer>
+	
 );
