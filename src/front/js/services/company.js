21da00e03016057489a12606746c841b03bd777c
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2023-05-02 12:07:07 +0200

    Vista todas empresas (#36)
    * all companies page
    
    * vista completa

--- src/front/js/services/company.js
+++ src/front/js/services/company.js
@@ -1,10 +1,10 @@
-import { URL } from ".";
+import {URL} from ".";
 
 export const companyRegister = async (data) => {
 
   try {
     const response = await fetch(`${URL}/company`, {
-
+      
       method: "POST",
       body: JSON.stringify(data),
       headers: {"Content-Type": "application/json"},
@@ -12,8 +12,19 @@ export const companyRegister = async (data) => {
     });
     //console.log(data)
     //console.log("status",response.status)
-
   } catch (error) {
-    console.log("Error en post:", error); 
+    console.log("Error en post:", error);
+  }
+};
+
+export const GetAllCompanies = async () => {
+  try {
+    const response = await fetch(`${URL}/company`, {
+      method: "GET",
+      redirect: "follow",
+    });
+    return await response.json();
+  } catch (error) {
+    console.log("Error en get:", error);
   }
 };
--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,5 @@
 
-export const URL = "https://3001-apacheco82-jobshood-o0q76xews1d.ws-eu96.gitpod.io/api";
+export const URL = "https://3001-apacheco82-jobshood-t4u2dobt2d1.ws-eu96.gitpod.io/api";
 
 
 export * from "../services/user.js";
