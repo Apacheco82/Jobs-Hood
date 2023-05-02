f6bf184543fef5f2036bc653bca7023eaff0051e
Ana Pacheco <kornetera@gmail.com>
2023-05-02 08:02:45 +0000

    all companies page

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,5 @@
 
-export const URL = "https://3001-apacheco82-jobshood-u3jp7erozqh.ws-eu96.gitpod.io/api";
+export const URL = "https://3001-apacheco82-jobshood-t4u2dobt2d1.ws-eu96.gitpod.io/api";
 
 
 export * from "../services/user.js";
