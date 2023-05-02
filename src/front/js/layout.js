80e0860dcee4975f6e5508d4ea812abc6ad2093e
Ana Pacheco <kornetera@gmail.com>
2023-05-02 12:03:25 +0000

    vista de todos los abogados

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -12,6 +12,7 @@ import { Profile } from "./pages/profile-worker.jsx";
 import { RegisterCompany } from "./pages/form-register-company.jsx";
 import { CompanyProfile } from "./pages/company-profile.jsx";
 import { LawyerProfile } from "./pages/lawyer-profile.jsx";
+import { AllLawyers } from "./pages/all-lawyers";
 
 
 const Layout = () => {
@@ -32,6 +33,7 @@ const Layout = () => {
                         <Route element={<CompanyProfile />} path="/company/profile" />
                         <Route element={<LawyerProfile />} path="/lawyer/:id" /> 
                         <Route element={<LawyerProfile />} path="/lawyer/profile" />
+                        <Route element={<AllLawyers />} path="/lawyers" />
                         <Route element={<RegistroWorker />} path="/register/worker" />
                         <Route element={<RegisterCompany />} path="/register/company" />
                         <Route element={<RegistroLawyer />} path="/register/lawyer" />
