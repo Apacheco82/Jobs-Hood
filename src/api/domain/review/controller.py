6edb63f587a1a37c11f3c8adabcc94cb2ac04cdf
Ana Pacheco <kornetera@gmail.com>
2023-05-03 10:43:08 +0000

    comentarios

--- src/api/domain/review/controller.py
+++ src/api/domain/review/controller.py
@@ -8,8 +8,8 @@ def get_reviews():
 
 def post_review(user, data):
     if user['role'] == "User": #se comprueba que el usuario logado es de tipo user, por eso le pasamos user_logged.serialize() en route como param
-        receiver = get_single_user(data['receiver_id'])
-        receiver_serialized = receiver.serialize()
+        receiver = get_single_user(data['receiver_id']) #nos traemos el usuario al que le estamos haciendo la review llamando a la funcion get_single_user de User
+        receiver_serialized = receiver.serialize() #serializamos el usuario recibido porque tenemos que acceder a su rol
         if receiver_serialized['role'] != "User": #para controlar que solo se puedan hacer reviews a abogados y empresas
             return Repository.post_review(data['receiver_id'], data['author_id'], data['rating'], data['text']) #pasamos los campos necesarios a repository
         return Response.response_error("no puedes hacer reseñas a otro usuario", 400)
