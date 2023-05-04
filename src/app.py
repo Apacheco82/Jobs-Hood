e005c52d7dddcda3ade295c5eda1aedb9f2bfe2a
Ana Pacheco <kornetera@gmail.com>
2023-05-04 18:28:40 +0000

    changes in relationships

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ from api.routes import api
 from api.domain.company.route import company_bp
 from api.domain.lawyer.route import lawyer_bp
 from api.domain.review.route import review_bp
+from api.domain.question.route import question_bp
+from api.domain.question_comment.route import question_comment_bp
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import  get_jwt_identity, jwt_required, JWTManager
@@ -54,6 +56,8 @@ app.register_blueprint(api_user.api, url_prefix='/api/user')
 app.register_blueprint(company_bp, url_prefix='/api/company')
 app.register_blueprint(lawyer_bp, url_prefix='/api/lawyer')
 app.register_blueprint(review_bp, url_prefix='/api/review')
+app.register_blueprint(question_bp, url_prefix='/api/question')
+app.register_blueprint(question_comment_bp, url_prefix='/api/question/comment')
 
 
 
