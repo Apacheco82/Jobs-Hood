7a9f15823b5462d46273bafa343f926e3b502bd3
Ana Pacheco <kornetera@gmail.com>
2023-05-09 20:05:44 +0000

    back_populates

--- src/api/models/company.py
+++ src/api/models/company.py
@@ -6,6 +6,7 @@ class Company(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='company')
     address = db.Column(db.String(100), nullable=False)
     province = db.Column(db.String(100), nullable=False)
     cp = db.Column(db.Integer(), nullable=False)
--- src/api/models/lawyer.py
+++ src/api/models/lawyer.py
@@ -6,6 +6,7 @@ class Lawyer(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', back_populates='lawyer')
     address = db.Column(db.String(100), nullable=False)
     province = db.Column(db.String(100), nullable=False)
     cp = db.Column(db.Integer(), nullable=False)
