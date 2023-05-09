a4ca8eaa4847759881a43d344da3c0b35b8002c2
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2023-05-09 20:31:24 +0200

    refactorizacion de componentes (#45)

--- src/front/js/pages/form-register-lawyer.jsx
+++ src/front/js/pages/form-register-lawyer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { registerLawyer } from "../services/lawyer.js";
 import { useNavigate } from "react-router-dom";
-import { Province } from "../component/form-province.jsx";
+import  Form  from "../component/Form.jsx";
 
 export const RegistroLawyer = () => {
 
@@ -46,143 +46,12 @@ export const RegistroLawyer = () => {
 	}
 
 	return (
-		<React.Fragment>
-
-			<div className="container text-center mt-5">
-				<h2>CREAR NUEVA CUENTA</h2>
-				<h5>Accede a todos los servicios de Jobs Hood !</h5>
-			</div>
-			<div className="container mt-5">
-				<h4>Datos de Acceso</h4>
-				<form onSubmit={handleSubmit} onChange={handleChange}>
-					<div className="row align-items-start my-3">
-						<div className="col">
-							<label
-								htmlFor="form-register-worker"
-								className="form-label">
-								Nombre
-							</label>
-							<input type="text"
-								className="form-control rounded-0"
-								name="name"
-								placeholder="Nombre"
-								maxLength="80"
-								value={form.name}
-								required
-							/>
-						</div>
-						<div className="col">
-							<label
-								htmlFor="inputPassword6"
-								className="form-label">
-								Contraseña
-							</label>
-							<input type="password"
-
-								className="form-control rounded-0"
-								name="password"
-								aria-labelledby="passwordHelpInline"
-								placeholder="Debe tener entre 8-20 caracteres."
-								value={form.password}
-								maxLength="250"
-								required
-							/>
-						</div>
-					</div>
-					<div className="row align-items-end my-3">
-						<div className="col">
-							<label
-								htmlFor="form-register-worker"
-								className="form-label">
-								Dirección Email
-							</label>
-							<input
-								type="email"
-
-								className="form-control rounded-0"
-								name="email"
-								placeholder="name@example.com"
-								value={form.email}
-								maxLength="250"
-								required
-							/>
-						</div>
-						<div className="col">
-							<label
-								htmlFor="form-register-worker"
-								className="form-label">
-								Dirección 
-							</label>
-							<input
-								type="text"
-
-								className="form-control rounded-0"
-								name="address"
-								value={form.address}
-								maxLength="100"
-								required
-							/>
-						</div>
-					</div>
-					<div className="row align-items-end my-3">
-						<div className="col">
-
-							<label htmlFor="form-register-company" className="form-label">
-								Provincia
-							</label>
-							<Province value={form.province} name="province" />
-
-						</div>
-						<div className="col">
-							<label htmlFor="form-register-company" className="form-label">
-								CIF
-							</label>
-							<input
-								type="text"
-								name="cif"
-								value={form.cif}
-								className="form-control rounded-0"
-								placeholder="CIF"
-								maxLength="10"
-								required
-							/>
-						</div>
-						</div>
-						<div className="row align-items-end my-3">
-							<div className="col">
-								<label
-									htmlFor="form-register-worker"
-									className="form-label">
-									Código postal
-								</label>
-								<input
-									type="number"
-									className="form-control rounded-0"
-									name="cp"
-									value={form.cp}
-									required
-								/>
-							</div>
-							<div className="col">
-								<label
-									htmlFor="form-register-worker"
-									className="form-label">
-									Número de colegiado
-								</label>
-								<input
-									type="text"
-									className="form-control rounded-0"
-									name="col_number"
-									value={form.col_number}
-									required
-								/>
-							</div>
-						</div>
-						<input type="submit" className="btn btn-dark mx-3 rounded-0" value="Registrarme"></input>
-				</form>
-			</div>
-
-		</React.Fragment >
+<Form
+  userType="lawyer" 
+  form={form}
+  handleChange={handleChange}
+  handleSubmit={handleSubmit}
+/>		
 
 	);
 };
--- src/front/js/pages/form-register-worker.jsx
+++ src/front/js/pages/form-register-worker.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 //import { Navbar } from ".././component/navbar";
 import { Context } from "../store/appContext";
 import { registerUser } from "../services";  
-
+import FormUser from "../component/FormUser.jsx";
 
 
 export const RegistroWorker = () => {
@@ -35,46 +35,12 @@ export const RegistroWorker = () => {
 	}
 
 	return (
-		<React.Fragment>
-		
-			<div className="container text-center mt-5">
-				<h2>CREAR NUEVA CUENTA</h2>
-				<h5>Accede a todos los servicios de Jobs Hood !</h5>
-			</div>
-			<div className="container mt-5">
-				<h4>Datos de Acceso</h4>
-				<form onChange={handleChange} onSubmit={handleSubmit}>
-					<div className="row align-items-start my-3">
-						<div className="col">
-							<label htmlFor="form-register-worker" className="form-label">Nombre de Usuario</label>
-							<input type="text" name="user_name" className="form-control rounded-0" placeholder="Usuario"  maxLength="20" required />
-						</div>
-						<div className="col">
-							<label  htmlFor="inputPassword6" className="form-label">Contraseña</label>
-							<input type="password" name="password" className="form-control rounded-0" aria-labelledby="passwordHelpInline" placeholder="Debe tener entre 8-20 caracteres." required />
-						</div>
-					</div>
-                    <div className="row align-items-start my-3">
-						<div className="col">
-							<label htmlFor="form-register-worker" className="form-label">Nombre</label>
-							<input type="text" name="name" className="form-control rounded-0" pattern="^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]*$" maxLength="20" required />
-						</div>
-						<div className="col">
-							<label htmlFor="form-register-worker" className="form-label">Apellidos</label>
-							<input type="text" name="last_name" className="form-control rounded-0"  pattern="^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ\s]*$" maxLength="40"  />
-						</div>
-					</div>
-					<div className="row align-items-end my-3">
-						<div className="col">
-							<label  htmlFor="form-register-worker" className="form-label">Dirección Email</label>
-							<input type="email" name="email" className="form-control rounded-0" placeholder="name@example.com" required />
-						</div>
-					</div>
-					<input type="submit" className="btn btn-dark mx-3  rounded-0" value="Registrarme"></input>
-				</form>
-			</div>
-
-		</React.Fragment >
-
-	);
+		<>
+		<FormUser 
+		  handleChange={handleChange}
+		  handleSubmit={handleSubmit} 
+		/>
+		</>
+	  )
 };
+
