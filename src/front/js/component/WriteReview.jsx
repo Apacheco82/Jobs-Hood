26e3d63946185e63c848caf38c42f182b816c7eb
Ana Pacheco <kornetera@gmail.com>
2023-05-11 12:02:43 +0000

    fix error

--- src/front/js/component/WriteReview.jsx
+++ src/front/js/component/WriteReview.jsx
@@ -50,7 +50,7 @@ const WriteReview = ({reviewChange, reviewSubmit}) => {
                   className="form-control"
                   id="exampleFormControlTextarea1"
                   rows="3"
-                  maxlength="700"
+                  maxLength="700"
                   required
                 ></textarea>
               </div>
