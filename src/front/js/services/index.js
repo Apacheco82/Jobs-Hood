ea00af990128a8d03171716c77ff50870edca807
Ana Pacheco <kornetera@gmail.com>
2023-05-17 11:23:09 +0000

    botón de volver y alert de usuario creado

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,4 +1,4 @@
 export const URL =
-  "https://3001-apacheco82-jobshood-5kvrwrn3r0w.ws-eu97.gitpod.io/api";
+  "https://3001-apacheco82-jobshood-byyguscl75u.ws-eu97.gitpod.io/api";
 
 export * from "../services/user.js";
