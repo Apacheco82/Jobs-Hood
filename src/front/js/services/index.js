d6f35c41a41498ce64a96c9583f80e37163cd7c5
Hedonista17 <andreaynereapiza@gmail.com>
2023-05-17 20:07:43 +0000

    se hace pull de lo ultimo en rama DEVELOP

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,5 @@
 
-  "https://3001-apacheco82-jobshood-ed0bn6ci3el.ws-eu97.gitpod.io/api";
+export const URL = "https://3001-apacheco82-jobshood-ed0bn6ci3el.ws-eu97.gitpod.io/api";
 
 
 export * from "../services/user.js";
