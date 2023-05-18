5471d336c693c0ed0011825f1d2fc76d8b838fe1
Hedonista17 <andreaynereapiza@gmail.com>
2023-05-18 18:31:08 +0000

    fvdvw guardado

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,5 @@
 
-export const URL = "https://3001-apacheco82-jobshood-ed0bn6ci3el.ws-eu97.gitpod.io/api";
+export const URL = "https://3001-apacheco82-jobshood-e05ncqp1hn3.ws-eu97.gitpod.io/api";
 
 
 export * from "../services/user.js";
