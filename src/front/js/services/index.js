3dc0e6cee7b726a1d5a706e0a072698b6274030d
Ana Pacheco <kornetera@gmail.com>
2023-05-20 10:58:24 +0000

    url

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,3 @@
-export const URL =
-  "https://3001-apacheco82-jobshood-zzmfg7r5c6b.ws-eu97.gitpod.io/api";
-
+export const URL = `${process.env. BACKEND_URL}/api`;
 
 export * from "../services/user.js";
