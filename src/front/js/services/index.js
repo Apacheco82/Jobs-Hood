900f43a63ce3f6e7b3ed55e2ace902278e28343a
Andrea Fernandez <120684452+Hedonista17@users.noreply.github.com>
2023-05-23 00:20:50 +0200

    Update index.js

--- src/front/js/services/index.js
+++ src/front/js/services/index.js
@@ -1,5 +1,4 @@
-export const URL =
-  "https://3001-apacheco82-jobshood-fo66kkixbpa.ws-us97.gitpod.io/api";
+export const URL = `${process.env. BACKEND_URL}/api`;
 
 
 export * from "../services/user.js";
