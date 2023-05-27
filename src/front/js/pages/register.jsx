0967a59eff441e3db2005d73d8ecda2e678715dc
Andrea Fernandez <120684452+Hedonista17@users.noreply.github.com>
2023-05-27 16:34:37 +0200

    Finalizado registro estilos (#80)

--- src/front/js/pages/register.jsx
+++ src/front/js/pages/register.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {Navbar} from "../component/navbar.js";
+import { Link } from "react-router-dom";
+import { Navbar } from "../component/navbar.js";
 import "../../styles/register.css";
 
 export const Register = () => {
@@ -9,16 +9,18 @@ export const Register = () => {
       <Navbar />
       <div className="register">
         <div className="container text-center mt-5">
-          <h1> Bienvenido a Jobs Hood!</h1>
+          <h1> Bienvenido a <strong>Jobs Hood!</strong> </h1>
           <h3>¿A que area pertenece? </h3>
 
-          <div className="row my-5">
+          <div className="row my-5 ">
             <div className="col mx-4">
               <Link to="lawyer">
-                <div className="card w-100">
+                <div id="card" className="card w-100">
                   <div className="card-body">
-                    <h5 className="card-title">Registrarme como Abogado/a</h5>
-                    <i className="fa-solid fa-gavel fa-2xl"></i>
+                    <h5 className="card-title">Soy un Abogado/a</h5>
+                    <span id="iconos" className="material-symbols-outlined">
+                      balance
+                    </span>
                   </div>
                 </div>
               </Link>
@@ -26,12 +28,14 @@ export const Register = () => {
 
             <div className="col mx-4">
               <Link to="worker">
-                <div className="card w-100">
+                <div id="card" className="card w-100">
                   <div className="card-body">
-                    <h5 className="card-title">
-                      Registrarme como Trabajador/a
+                    <h5 className="card-title ">
+                      Soy un Trabajador/a
                     </h5>
-                    <i className="fa-solid fa-user-group fa-2xl"></i>
+                    <span id="iconos" className="material-symbols-outlined">
+                      group
+                    </span>
                   </div>
                 </div>
               </Link>
@@ -39,10 +43,12 @@ export const Register = () => {
 
             <div className="col mx-4">
               <Link to="company">
-                <div className="card w-100">
+                <div id="card" className="card w-100">
                   <div className="card-body">
-                    <h5 className="card-title">Registrarme como Empresa</h5>
-                    <i className="fa-regular fa-building fa-2xl"></i>
+                    <h5 className="card-title"> Soy una  Empresa</h5>
+                    <span id="iconos" className="material-symbols-outlined">
+                      apartment
+                    </span>
                   </div>
                 </div>
               </Link>
