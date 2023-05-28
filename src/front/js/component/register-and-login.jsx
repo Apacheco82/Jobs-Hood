3287eddc12fb79b39d3d5c2918d477ae36345096
Andrea Fernandez <120684452+Hedonista17@users.noreply.github.com>
2023-05-28 14:08:20 +0200

    Finalizado estilos (#83)

--- src/front/js/component/register-and-login.jsx
+++ src/front/js/component/register-and-login.jsx
@@ -1,28 +1,27 @@
-
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-
-
-
 const ButtonsNavbar = () => {
-
     return (
         <>
             <ul className="navbar-nav d-flex gap-5 me-2 mb-2 mb-lg-0">
                 <Link to="/register">
-                    <button type="button" className="btn-navbar"><i className="fa-solid fa-bookmark mx-1"></i> Registrarme</button>
+                    <button type="button" className="btn-navbar d-flex ">
+                        <span className="material-symbols-outlined mx-1">
+                            edit
+                        </span>Registrarme
+                    </button>
                 </Link>
-
 
                 <Link to="/login">
-                    <button type="button" className="btn-navbar"><i className="fa-solid fa-user mx-2 fa-lg"></i>Iniciar sesión</button>
+                    
+                    <button type="button" className="btn-navbar d-flex ">
+                    <span className="material-symbols-outlined mx-1">account_circle</span> Iniciar sesión
+                    </button>
                 </Link>
-
             </ul>
-        </>)
-}
-
+        </>
+    );
+};
 
 export default ButtonsNavbar;
